58582020988b6fba1e85adcb70f66110d2c14e62
shuhongfan <shuhongfan@live.com>
2021-09-12 09:59:59 +0800

    2021.9.12

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+//
+// Created by shuho on 2021/9/11.
+//
+
+int main(){
+    int a=10;
+
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    system("pause");
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
